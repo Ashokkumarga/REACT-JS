5bc4d295c294027b6e75bfdeb3e752148b421678
ashokkumarga <ashokkumarga0@gmail.com>
2022-11-29 18:44:07 +0530

    29 nov

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import Project from "./Project";
+import Navbar from "./Udemy/Navbar";
+// import Project from "./Project";
 import Testing from "./Test/Testing";
-import ClassA from "./Test/Class/ClassA";
+// import ClassA from "./Test/Class/ClassA";
+import Product from "./Product/Product";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>App Component</h1>
-      <hr />
       <Testing />
-      <Project />
-      <ClassA />
+      {/* <Project />
+      <ClassA />*/}
+      <Product />
     </div>
   );
 }
